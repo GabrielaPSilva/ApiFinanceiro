4960934cb634ad513adfd1737cc51ba210cd190e
Gabriela Pinheiro <gabi.pinheiro.silva@gmail.com>
2024-01-08 20:41:50 -0300

    Adicionado Swagger e Script SQL Server

--- APIFinanceiro/Program.cs
+++ APIFinanceiro/Program.cs
@@ -1,24 +1,73 @@
+using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "API Financeiro",
+        Description = "",
+    });
+});
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy",
+                       builder =>
+                       {
+                           builder.AllowAnyMethod()
+                                  .AllowAnyHeader()
+                                  .AllowAnyOrigin();
+                       });
+});
+
+builder.Services.Configure<ForwardedHeadersOptions>(options =>
+{
+    options.ForwardedHeaders =
+        ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+}); 
 
 var app = builder.Build();
+
+app.Use((context, next) =>
+{
+    context.Request.Scheme = "https";
+    return next(context);
+});
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwagger(options =>
+    {
+        options.PreSerializeFilters.Add((swagger, req) =>
+        {
+            swagger.Servers = new List<OpenApiServer>() { new OpenApiServer() { Url = $"https://{req.Host}" } };
+        });
+    });
+
+    app.UseDeveloperExceptionPage();
+
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    });
 }
 
-app.UseHttpsRedirection();
+//Recomendado para processar corretamente os cabeçalhos encaminhados
+app.UseForwardedHeaders();
 
+app.UseRouting();
+
+//Configurar autorização na aplicação
 app.UseAuthorization();
 
 app.MapControllers();
