a68975730496bf8eb32492e2977a0b2dc57df1eb
GabbrielK <gabbriel.kronbauer@gmail.com>
2024-01-28 22:24:06 -0300

    Incluindo validações dos campos da tabela investimento Model

--- APIFinanceiro.Model/Entities/InvestimentoModel.cs
+++ APIFinanceiro.Model/Entities/InvestimentoModel.cs
@@ -14,5 +14,54 @@ namespace APIFinanceiro.Model.Entities
         public Decimal Saldo { get; set; }
         public Decimal ValorRendimento { get; set; }
         public Decimal ValorFinal { get; set; }
+
+        public bool IsValid(out string mensagemErro)
+        {
+            bool isValid = true;
+            StringBuilder sbMensagemErro = new StringBuilder();
+
+            sbMensagemErro.Clear();
+
+            string mensagemErroDecimal = string.Empty;
+            isValid = isValid && ValidarDecimal(out mensagemErroDecimal);
+            sbMensagemErro.Append(mensagemErroDecimal);
+
+            mensagemErro = sbMensagemErro.ToString();
+
+            return isValid;
+        }
+        private bool ValidarDecimal(out string mensagemErro)
+        {
+            bool isValidSaldo = true;
+            bool isValidValorRendimento = true;
+            bool isValidValorFinal = true;
+            isValidSaldo = Saldo!.GetType() == typeof(decimal);
+            isValidValorRendimento = ValorRendimento!.GetType() == typeof(decimal);
+            isValidValorFinal = ValorFinal.GetType() == typeof(decimal);
+
+            mensagemErro = string.Empty;
+
+            if (!isValidSaldo)
+            {
+                mensagemErro = "O Saldo deve ser um decimal.\n";
+                return isValidSaldo;
+            }
+
+
+            if (!isValidValorRendimento)
+            {
+                mensagemErro = "O Valor de rendimento deve ser um decimal.\n");
+                return isValidValorRendimento;
+            }
+
+
+            if (!isValidValorFinal)
+            {
+                mensagemErro = "O Valor de rendimento deve ser um decimal.\n";
+                return isValidValorFinal;
+            }
+
+            return true;
+        }
     }
 }
