0a577d9d14f4a1271198c2926b3ba22eb6bb4761
Gabriela Pinheiro <gabi.pinheiro.silva@gmail.com>
2024-03-11 18:08:56 -0300

    Adicionado ci-cd.yml

--- APIFinanceiro.Business/obj/Debug/net7.0/APIFinanceiro.Business.AssemblyInfo.cs
+++ APIFinanceiro.Business/obj/Debug/net7.0/APIFinanceiro.Business.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APIFinanceiro.Business")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd5da0214126978a1481fd35cc7fe18eadf5296d")]
 [assembly: System.Reflection.AssemblyProductAttribute("APIFinanceiro.Business")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APIFinanceiro.Business")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- APIFinanceiro.Data/obj/Debug/net7.0/APIFinanceiro.Data.AssemblyInfo.cs
+++ APIFinanceiro.Data/obj/Debug/net7.0/APIFinanceiro.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APIFinanceiro.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd5da0214126978a1481fd35cc7fe18eadf5296d")]
 [assembly: System.Reflection.AssemblyProductAttribute("APIFinanceiro.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APIFinanceiro.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
